b985a27c212e375cac536f3839fa012892f55afe
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2025-01-29 16:13:29 -0300

    adicão do banco de dados

--- routes.php
+++ routes.php
@@ -1,8 +1,8 @@
 <?php
 
 $controller = str_replace('/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
-if (!$controller) $controller = 'index';
 
+if (!$controller) $controller = 'index';
 if (! file_exists("controllers/{$controller}.controller.php")) {
     abort(404);
 }
