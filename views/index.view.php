b45c2367a684b2a0c229fdbb2efb8812f69525ba
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2025-02-10 23:17:14 -0300

    feat: primeira model

--- views/index.view.php
+++ views/index.view.php
@@ -12,16 +12,16 @@
             <div class="flex">
                 <div class="w-1/3 bg-stone-800">imagens</div>
                 <div>
-                    <a class="font-semibold" href="livro?id=<?= $livros['id'] ?>" hover: underline>
-                        <?= $livros['titulo'] ?>
+                    <a class="font-semibold" href="livro?id=<?= $livros->id ?>" hover: underline>
+                        <?= $livros->titulo ?>
                     </a>
 
-                    <div class="text-xs italic"><?= $livros['autor'] ?></div>
+                    <div class="text-xs italic"><?= $livros->autor ?></div>
                     <div class="text-xs italic">⭐⭐⭐⭐⭐(3 avaliações)</div>
                 </div>
 
             </div>
-            <div class="text-xs italic"><?= $livros['descricao'] ?></div>
+            <div class="text-xs italic"><?= $livros->descricao ?></div>
         </div>
     <?php endforeach ?>
 
